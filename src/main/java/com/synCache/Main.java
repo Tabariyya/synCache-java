b3db32f8724a1aa9718074fec4a6e58c8dab9aa6
waleedshanaa <waleed_shanaa@icloud.com>
2025-08-29 00:05:06 +0300

    added linux/arm/.so

--- src/main/java/com/synCache/Main.java
+++ src/main/java/com/synCache/Main.java
@@ -5,7 +5,7 @@ import com.synCache.cache.NativeController;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        NativeController ctrl = new NativeController("amqp://guest:guest@localhost:5672/", 100);
+        NativeController ctrl = new NativeController("amqp://guest:guest@91.93.135.176:5672/", 100);
         NativeCacheEntry e = new NativeCacheEntry("1", "ns", "value", /*ttl*/ null);
         ctrl.set(e, null);
         System.out.println("sleeping");
@@ -14,7 +14,6 @@ public class Main {
         System.out.println("value=" + v);
         Thread.sleep(10000);
 
-//        ctrl.evict("ns", "1");
     }
 
 }
