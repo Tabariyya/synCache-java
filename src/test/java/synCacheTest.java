45c71a173802df35a95fe925bc778db1852da7cc
waleedshanaa <waleed_shanaa@icloud.com>
2025-09-04 00:04:41 +0300

    created templet

--- src/test/java/synCacheTest.java
+++ src/test/java/synCacheTest.java
@@ -1,5 +1,19 @@
+import com.synCache.cache.NativeCacheEntry;
+import com.synCache.cache.NativeController;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class synCacheTest {
+
+    @Test
+    void test1() throws InterruptedException {
+        NativeController ctrl = new NativeController("amqp://guest:guest@91.93.135.176:25672/", 100);
+        NativeCacheEntry e = new NativeCacheEntry("1", "ns", "value", /*ttl*/ null);
+        ctrl.set(e);
+        System.out.println("sleeping");
+        Thread.sleep(10000);
+        String v = ctrl.get("ns", "1");
+        System.out.println("value=" + v);
+        Thread.sleep(10000);
+
+    }
 }
