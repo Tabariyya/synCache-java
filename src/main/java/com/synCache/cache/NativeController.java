d969f761680f3a206cad0e3873d6a7d7421878cc
waleedshanaa <waleed_shanaa@icloud.com>
2025-09-06 02:50:51 +0300

    added serilizaiın

--- src/main/java/com/synCache/cache/NativeController.java
+++ src/main/java/com/synCache/cache/NativeController.java
@@ -1,12 +1,16 @@
 package com.synCache.cache;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class NativeController implements AutoCloseable {
     static {
         System.load(LibraryLoader.getLibraryPath());
     }
 
     private long nativeHandle;
+    private final ObjectMapper mapper = new ObjectMapper();
 
     public NativeController(String rabbitMqConnectionUri, long maxNoOfEntries) {
         this.nativeHandle = nCreate(rabbitMqConnectionUri, maxNoOfEntries);
@@ -19,8 +23,15 @@ public class NativeController implements AutoCloseable {
     /**
      * Returns value string or null if not present
      */
-    public String get(String nameSpace, String id) {
-        return nGet(nativeHandle, nameSpace, id);
+    public <T> T get(String nameSpace, String id, Class<T> clazz) {
+        String json = nGet(nativeHandle, nameSpace, id);
+
+        // mapper.readValue takes a String and a Class<T>
+        try {
+            return mapper.readValue(json, clazz);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void evict(String nameSpace, String id) {
