bf2fc6c6c30dd1df43f5bbeb4213a66c4c87097d
waleedshanaa <waleed_shanaa@icloud.com>
2025-10-03 19:08:38 +0300

    ..

--- src/main/java/com/synCache/Controller.java
+++ src/main/java/com/synCache/Controller.java
@@ -1,24 +1,23 @@
 package com.synCache;
 
 
-public class Controller implements AutoCloseable {
+public class Controller {
     static {
         System.load(LibraryLoader.getLibraryPath());
     }
 
-    private long nativeHandle;
 
     public Controller(String rabbitMqConnectionUri, long maxNoOfEntries, boolean async) {
-        this.nativeHandle = create(rabbitMqConnectionUri, maxNoOfEntries, async);
+        nCreate(rabbitMqConnectionUri, maxNoOfEntries, async);
     }
 
     public void set(CacheEntry entry) {
-        set(nativeHandle, entry.getNameSpace(), entry.getId(), entry.getValue(), entry.getTtl());
+        nSet(entry.getNameSpace(), entry.getId(), entry.getValue(), entry.getTtl());
     }
 
 
     public <T> T get(String nameSpace, String id, Class<T> clazz) {
-        byte[] json = get(nativeHandle, nameSpace, id);
+        byte[] json = nGet(nameSpace, id);
         if (json == null) {
             return null;
         }
@@ -26,26 +25,16 @@ public class Controller implements AutoCloseable {
     }
 
     public void evict(String nameSpace, String id) {
-        evict(nativeHandle, nameSpace, id);
+        nEvict(nameSpace, id);
     }
 
-    @Override
-    public void close() {
-        long h = nativeHandle;
-        if (h != 0) {
-            destroy(h);
-            nativeHandle = 0;
-        }
-    }
 
     // ---- Native declarations ----
-    private static native long create(String uri, long maxEntries, boolean async);
+    private native void nCreate(String uri, long maxEntries, boolean async);
 
-    private static native void destroy(long handle);
+    private native void nSet(String nameSpace, String id, byte[] value, Long ttl);
 
-    private static native void set(long controllerHandle, String nameSpace, String id, byte[] value, Long ttl);
+    private native byte[] nGet(String nameSpace, String id);
 
-    private static native byte[] get(long controllerHandle, String nameSpace, String id);
-
-    private static native void evict(long controllerHandle, String nameSpace, String id);
+    private native void nEvict(String nameSpace, String id);
 }
